bf506ce62d45f1f14cbe17eb6507e55859b1d87d
ratek20 <filippo.cloud.user@gmail.com>
2025-12-30 22:43:42 +0100

    feat: initial commit

---
+++ src/main/java/com/mcp/factorial_mcp_server/Application.java
@@ -0,0 +1,13 @@
+package com.mcp.factorial_mcp_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
