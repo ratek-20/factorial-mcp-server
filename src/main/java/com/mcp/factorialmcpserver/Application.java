edb25d78571a9d2711733dbf9efa5a9342d0051b
ratek20 <filippo.cloud.user@gmail.com>
2026-01-17 11:05:06 +0100

    feat: intercept EOF from MCP client to force the shutdown of the JVM

--- src/main/java/com/mcp/factorialmcpserver/Application.java
+++ src/main/java/com/mcp/factorialmcpserver/Application.java
@@ -2,12 +2,48 @@ package com.mcp.factorialmcpserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.lang.NonNull;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootApplication
 public class Application {
 
 	static void main(String[] args) {
+		System.setIn(new EofDetectingInputStream(System.in));
+		/*
+		Intercepts the EOF signal from the MCP client to handle connection closure.
+		Required because the JDK Oauth2 Http Server's non-daemon dispatcher thread prevents automatic JVM shutdown.
+		Explicitly calls System.exit(0) to ensure the Docker container stops when the MCP client disconnects.
+		 */
+
 		SpringApplication.run(Application.class, args);
+	}
+
+	private static class EofDetectingInputStream extends FilterInputStream {
+		protected EofDetectingInputStream(InputStream in) {
+			super(in);
+		}
+
+		@Override
+		public int read() throws IOException {
+			int result = super.read();
+			if (result == -1) {
+				System.exit(0);
+			}
+			return result;
+		}
+
+		@Override
+		public int read(@NonNull byte[] bytes, int off, int len) throws IOException {
+			int result = super.read(bytes, off, len);
+			if (result == -1) {
+				System.exit(0);
+			}
+			return result;
+		}
 	}
 
 }
